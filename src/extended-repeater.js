f320abd519d336a6e7ede0ed2d0dfca6045ac952
pashaSok <https://github.com/pashaSok>
2020-03-22 18:08:59 +0300

    feat: create extended repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,13 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+	this.repeatTimes = options.repeatTimes !== undefined ? options.repeatTimes : 1;
+    this.separator = options.separator !== undefined ? String(options.separator) : '+';
+    this.addition = options.addition !== undefined ? String(options.addition) : '';
+    this.additionRepeatTimes = options.additionRepeatTimes != undefined ? options.additionRepeatTimes : 1;
+	this.additionSeparator = options.additionSeparator != undefined ? String(options.additionSeparator) : '+';
+	
+
+
+	let newStr = new Array(additionRepeatTimes).fill(addition).join(additionSeparator);
+	return new Array(repeatTimes).fill(str+newStr).join(separator);
 };
   
